0ae13d852d5f456cabb4603b567bed2921ab572b
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-10-14 19:19:09 -0300

    New Features

--- script.js
+++ script.js
@@ -28,49 +28,41 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "Tie";
     }
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return "Computer wins! 👀";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
         return "Player wins! 💪";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "Player wins! 💪";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    } else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
         return "Computer wins! 👀";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "Computer wins! 👀";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "Player wins! 💪";
     }
 }
 
 function game() {
     let playerTotal = 0;
-    let computerTotal = 0;    
+    let computerTotal = 0;
     alert("Welcome to the Game! 🎮\n\nPlease press 'OK' to start!")
     alert("🚨 Rules: Please write 'Rock', 'Paper' or 'Scissors'. The computer will do the same. \n\nThe result will be defined by:\n- Rock beats Scissors.\n- Paper beats Rock.\n- Scissors beat Paper.\n\nLet's play 5 rounds!")
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("🕹 Please, enter 'Rock', 'Paper', or 'Scissors':").toLowerCase().trim();
-        while(!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors" || playerSelection == "scissor" || playerSelection == "papers" || playerSelection == "rocks")) {
+        while (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors" || playerSelection == "scissor" || playerSelection == "papers" || playerSelection == "rocks")) {
             playerSelection = prompt("🔴 PLEASE ENTER A VALID VALUE.\n\nPlease, enter 'Rock', 'Paper', or 'Scissors':").toLowerCase().trim();
         }
         let computerSelection = computerPlay();
         let thisRoundResult = playRound(playerSelection, computerSelection.toLowerCase());
-        if(thisRoundResult.startsWith("Player wins")) {
+        if (thisRoundResult.startsWith("Player wins")) {
             playerTotal++;
-        } else if(thisRoundResult.startsWith("Computer wins")) {
+        } else if (thisRoundResult.startsWith("Computer wins")) {
             computerTotal++;
         }
         alert(`Computer election: ${computerSelection}.\n\nThis round: ${thisRoundResult}\n\nPlayer vs Computer: ${playerTotal} - ${computerTotal}`)
     }
     let winner;
-    if(playerTotal > computerTotal) {
+    if (playerTotal > computerTotal) {
         winner = "🕺 PLAYER";
     } else if (computerTotal > playerTotal) {
         winner = "🖥 COMPUTER"
     } else {
         winner = false;
     }
-    if(winner) {
+    if (winner) {
         alert(`🏁 And the final result is:\n\n🥁 🥁 🥁 🥁 🥁\n\n${winner} WINS! 🏅`)
     } else {
         alert("And the final result is:\n\nTIE!")
