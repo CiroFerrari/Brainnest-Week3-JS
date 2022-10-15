e9ff479eeebd1c24e7886319a6da45e0bd528f67
Franco Ciro Ferrari <francociroferrari@gmail.com>
2022-10-15 08:20:13 -0300

    New features

--- script.js
+++ script.js
@@ -41,7 +41,14 @@ function game() {
     alert("Welcome to the Game! 🎮\n\nPlease press 'OK' to start!")
     alert("🚨 Rules: Please write 'Rock', 'Paper' or 'Scissors'. The computer will do the same. \n\nThe result will be defined by:\n- Rock beats Scissors.\n- Paper beats Rock.\n- Scissors beat Paper.\n\nLet's play 5 rounds!")
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("🕹 Please, enter 'Rock', 'Paper', or 'Scissors':").toLowerCase().trim();
+        let playerSelection = prompt("🕹 Please, enter 'Rock', 'Paper', or 'Scissors':");
+        if(playerSelection == null) {
+            alert("See you the next time! 😊");
+            computerTotal = 5;
+            break;
+        } else {
+            playerSelection = playerSelection.toLowerCase().trim();
+        }
         while (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors" || playerSelection == "scissor" || playerSelection == "papers" || playerSelection == "rocks")) {
             playerSelection = prompt("🔴 PLEASE ENTER A VALID VALUE.\n\nPlease, enter 'Rock', 'Paper', or 'Scissors':").toLowerCase().trim();
         }
@@ -65,7 +72,7 @@ function game() {
     if (winner) {
         alert(`🏁 And the final result is:\n\n🥁 🥁 🥁 🥁 🥁\n\n${winner} WINS! 🏅`)
     } else {
-        alert("And the final result is:\n\nTIE!")
+        alert("And the final result is:\n\nDRAW!")
     }
 }
 game();
